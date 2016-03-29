a23cdd2eda003d9aeab2059f7063e4b725b8e430
Leela Gutta <gutta.dhanya@gmail.com>
2016-03-29 08:23:12 -0700

    Listing all the items

---
+++ list.js
@@ -0,0 +1,126 @@
+var introList=
+'<div class="intro">This is a list of some of the important learnings and lessons I learnt about design and web</div>'+
+// '<h4>Leela Gutta</h4>'
+'<div class="contactList"<ul>'+
+	'<li >LinkedIn</li>'+
+	'<li >Twitter</li>'+
+	'<li>Website</li>'+
+	'<li >Email</li></ul></div>';
+
+var designList = 
+'<div class="heading"><h1>DESIGN 101</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>What is Design?</li>'+
+'<li>How is Design important in Tech?</li>'+
+'<li>Design designation titles and differences</li>'+
+'<li>Design Principals</li>'+
+'<li><a href="http://blog.crew.co/native-app-vs-web-app/">'+
+'Native app Vs Web app</a></li>'+
+'<li><a href="https://css-tricks.com/design-systems-building-future/">'+
+'Design Systems</a></li>'+
+'<li><a href="http://www.designyourway.net/blog/resources/understanding-design-and-development-jobs-titles/">'+
+'understanding-design-and-development-jobs-titles</a></li>'+
+'</ul></div>'
+
+var uxDesign=
+'<div class="heading"><h1>UX DESIGN</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>What is UX design?</li>'+
+'<li>UX is not common sense</li>'+
+'<li>Difference between UX and UI design</li>'+
+'<li><a href="https://medium.com/@wnialloconnor/how-to-build-an-experience-map-5e55b7ee4f32#.h603ciorw">'+
+'How to build an experience map</a></li>'+
+'<li><a href="https://www.smashingmagazine.com/2011/09/the-s-m-a-r-t-user-experience-strategy/">'+
+'SMART userExperience Strategy</a></li></ul></div>';
+
+var colorList = 
+'<div class="heading"><h1>COLOR</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>What is color?</li>'+
+'<li>UI gradiants</li>'+
+'<li>IOS colors</li>'+
+'<li>Color history</li>'+
+'<li>Color in web</li></ul></div>';
+
+var typeList=
+'<div class="heading"><h1>TYPOGRAPHY</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>What is typography?</li>'+
+'<li>Best Type pairings</li>'+
+'<li>Understanding Type</li>'+
+'<li>Typography in Webdesign</li>'+
+'<li>Best books in typography</li>'+
+'<li>Typography in mobile</li></ul></div>';
+
+var jsList=
+'<div class="heading"><h1>JavaScript</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>Basics of JavaScript</li>'+
+'<li>Objects and Functions</li>'+
+'<li>How does memory work in JS</li>'+
+'<li>Scopes and clousers</li>'+
+'<li>Design Patterns</li>'+
+'<li>Jquery methods</li></ul></div>';
+
+var webList=
+'<div class="heading"><h1>WEB DESIGN</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>Responsive web design fundamentals - Udacity</li>'+
+'<li>Responsive images</li>'+
+'<li>What is viewport?</li>'+
+'<li><a href="http://learn.shayhowe.com/html-css/positioning-content/">'+
+'Positioning Content</a></li>'+
+'<li><a href="http://www.peterjthomson.com/2009/08/difference-between-marketing-and-design/">'+
+'Difference between marketing and Design</a></li></ul></div>';
+
+var htmlList=
+'<div class="heading"><h1>HTML</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>What is HTML & CSS</li>'+
+'<li>Cheat sheet for all the elements</li>'+
+'</ul></div>';
+
+var cssList=
+'<div class="heading"><h1>CSS</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>What is HTML & CSS</li>'+
+'<li><a href="http://www.w3schools.com/cssref/css_selectors.asp">'+
+'Cheat sheet for css selectors</a></li>'+
+'<li>Cheat sheet for all the elements</li>'+
+'<li>Css cascasing rules</li>'+
+'<li>Css display</li>'+
+'<li>Css flexbox</li>'+
+'<li>Animations</li>'+
+'<li>Box Model</li>'+
+'<li><a href="http://www.webdesignerdepot.com/2013/08/10-css-selectors-you-shouldnt-code-without/">10 useful Selectors</a></li>'+
+'<li><a href="https://css-tricks.com/how-nth-child-works/">How n-th child works</a></li>'+
+'<li><a href="https://css-tricks.com/useful-nth-child-recipies/">n-th child receipes</a></li>'+
+'<li><a href="https://css-tricks.com/child-and-sibling-selectors/">'+
+'child-and-sibling-selectors</a></li>'+
+'<li><a href="http://learnlayout.com/toc.html">'+
+'CSS Layout</a></li></ul></div>';	
+
+var frameworkList=
+'<div class="heading"><h1>FRAMEWORK</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>Css Animations</li>'+
+'<li>Custom Grid</li>'+
+'<li>Masonry Layout</li>'+
+'<li><a href="https://medium.com/@wnialloconnor/how-to-build-an-experience-map-5e55b7ee4f32#.h603ciorw">'+
+'How to build an experience map</a></li>'+
+'<li><a href="https://ole.michelsen.dk/blog/setup-local-web-server-apache-php-osx-yosemite.html">'+
+'Local WebServer Setup</a></li>'
+'<li>BootStrap</li></ul></div>';
+
+var list =[introList,designList,uxDesign,colorList,typeList,webList,htmlList,cssList,jsList,frameworkList];  
+
+
