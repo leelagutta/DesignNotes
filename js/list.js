9fa49a7ab6b6125f632cea4167b791b8272ca6e2
Leela Gutta <gutta.dhanya@gmail.com>
2016-04-13 21:19:40 -0700

    Adding content

--- js/list.js
+++ js/list.js
@@ -2,30 +2,46 @@ var introList=
 '<div class="intro"><p>These are few of the resources I always want to have in handy. This list will help anyone looking to kickstart their career in design in Tech.</p>';
 
 var designList = 
-'<div class="heading"><h1>DESIGN 101</h1></div>'+
+'<div class="heading"><h1>DESIGN</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Design Principals</li>'+
-'<li>Importance of design in Tech</li>'+
+'<li>Understanding what UX design is</li>'+
+'<li>Design and developement job titles</li>'+
 '<li><a target="_blank" href="https://css-tricks.com/design-systems-building-future/">'+
-'Design Systems</a></li>'+
+'Design and its role in Technology</a></li>'+
 '<li><a target="_blank" href="http://blog.crew.co/native-app-vs-web-app/">'+
-'Native app Vs Web app</a></li>'+
+'what is design Thinking</a></li>'+
 '<li><a target="_blank" href="http://www.designyourway.net/blog/resources/understanding-design-and-development-jobs-titles/">'+
-'Understanding design & developement job titles</a></li>'+
+'Design principals</a></li>'+
+'<li><a target="_blank" href="http://www.designyourway.net/blog/resources/understanding-design-and-development-jobs-titles/">'+
+'Best Design Books</a></li>'+
 '</ul></div>'
 
-var uxDesign=
-'<div class="heading"><h1>UX DESIGN</h1></div>'+
+var responsiveList=
+'<div class="heading"><h1>Responsive DESIGN</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>What is UX design?</li>'+
-'<li>UX is not common sense</li>'+
-'<li>Difference between UX and UI design</li>'+
+'<li>Responsive web design fundamentals - Udacity</li>'+
+'<li>Responsive images</li>'+
+'<li>What is viewport?</li>'+
+'<li><a href="http://learn.shayhowe.com/html-css/positioning-content/">'+
+'Positioning Content</a></li>'+
+'<li><a href="http://www.peterjthomson.com/2009/08/difference-between-marketing-and-design/">'+
+'Difference between marketing and Design</a></li></ul></div>';
+
+var researchTools=
+'<div class="heading"><h1>Research Tools</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>Task Models</li>'+
+'<li>Conent Audit</li>'+
+'<li>SiteMaps</li>'+
 '<li><a target="_blank"  href="https://medium.com/@wnialloconnor/how-to-build-an-experience-map-5e55b7ee4f32#.h603ciorw">'+
 'How to build an experience map</a></li>'+
 '<li><a target="_blank"  href="https://www.smashingmagazine.com/2011/09/the-s-m-a-r-t-user-experience-strategy/">'+
-'SMART userExperience Strategy</a></li></ul></div>';
+'SMART userExperience Strategy</a></li>'+
+'<li><a target="_blank"  href="https://www.smashingmagazine.com/2011/09/the-s-m-a-r-t-user-experience-strategy/">'+
+'User Personas</a></li></ul></div>';
 
 var colorList = 
 '<div class="heading"><h1>COLOR</h1></div>'+
@@ -41,9 +57,8 @@ var typeList=
 '<div class="heading"><h1>TYPOGRAPHY</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>What is typography?</li>'+
-'<li>Best Type pairings</li>'+
 '<li>Understanding Type</li>'+
+'<li>Best Type pairings</li>'+
 '<li>Typography in Webdesign</li>'+
 '<li>Best books in typography</li>'+
 '<li>Typography in mobile</li></ul></div>';
@@ -59,34 +74,11 @@ var jsList=
 '<li>Design Patterns</li>'+
 '<li>Jquery methods</li></ul></div>';
 
-var webList=
-'<div class="heading"><h1>WEB DESIGN</h1></div>'+
+var htmlcssList=
+'<div class="heading"><h1>Html & CSS</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Responsive web design fundamentals - Udacity</li>'+
-'<li>Responsive images</li>'+
-'<li>What is viewport?</li>'+
-'<li><a href="http://learn.shayhowe.com/html-css/positioning-content/">'+
-'Positioning Content</a></li>'+
-'<li><a href="http://www.peterjthomson.com/2009/08/difference-between-marketing-and-design/">'+
-'Difference between marketing and Design</a></li></ul></div>';
-
-var htmlList=
-'<div class="heading"><h1>HTML</h1></div>'+
-'<div class="line"></div>'+
-'<div class="list"><ul>'+
-'<li>What is HTML & CSS</li>'+
-'<li>Cheat sheet for all the elements</li>'+
-'</ul></div>';
-
-var cssList=
-'<div class="heading"><h1>CSS</h1></div>'+
-'<div class="line"></div>'+
-'<div class="list"><ul>'+
-'<li>What is HTML & CSS</li>'+
-'<li><a href="http://www.w3schools.com/cssref/css_selectors.asp">'+
-'Cheat sheet for css selectors</a></li>'+
-'<li>Cheat sheet for all the elements</li>'+
+'<li>Cheat sheet for css selectors</a></li>'+
 '<li>Css cascasing rules</li>'+
 '<li>Css display</li>'+
 '<li>Css flexbox</li>'+
@@ -100,26 +92,37 @@ var cssList=
 '<li><a href="http://learnlayout.com/toc.html">'+
 'CSS Layout</a></li></ul></div>';	
 
-var frameworkList=
-'<div class="heading"><h1>FRAMEWORK</h1></div>'+
+var diyworkList=
+'<div class="heading"><h1>DIY</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Css Animations</li>'+
+'<li>Custom Css Animations</li>'+
 '<li>Custom Grid</li>'+
-'<li>Masonry Layout</li>'+
-'<li><a href="https://medium.com/@wnialloconnor/how-to-build-an-experience-map-5e55b7ee4f32#.h603ciorw">'+
-'How to build an experience map</a></li>'+
+'<li>Custom Masonry Layout</li>'+
 '<li><a href="https://ole.michelsen.dk/blog/setup-local-web-server-apache-php-osx-yosemite.html">'+
 'Local WebServer Setup</a></li>'
 '<li>BootStrap</li></ul></div>';
+
+var frameworkList =
+'<div class="heading"><h1>Frameworks</h1></div>'+
+'<div class="line"></div>'+
+'<div class="list"><ul>'+
+'<li>Sass</li>'+
+'<li>Angular JS</li>'+
+'<li>Es Lint</li>'+
+'<li><a href="https://ole.michelsen.dk/blog/setup-local-web-server-apache-php-osx-yosemite.html">'+
+'Testing</a></li>'
+'<li>BootStrap</li></ul></div>';
+
 
 var contactList='<ul>'+
 	'<li><a target="_blank" href="https://www.linkedin.com/in/leelagutta"><img src="asserts/linkedin.png" height="30" width="30" alt="LinkedIn"></a></li>'+
 	'<li><a target="_blank" href="https://twitter.com/DhanyaGutta"><img src="asserts/twitter_bird.png" height="30" width="30" alt="Twitter"></a></li>'+
 	'<li><a target="_blank" href="https://dhanyagutta.com"><img src="asserts/email.png" height="30" width="30" alt="email"></a></li>'+
+	'<li><a target="_blank" href="https://dhanyagutta.com"><img src="asserts/medium.png" height="30" width="30" alt="medium"></a></li>'+
 	'</ul>';
 
-var list =[introList,designList,uxDesign,colorList,typeList,webList,htmlList,cssList,jsList,frameworkList];  
+var list =[introList,designList,responsiveList,researchTools,colorList,typeList,htmlcssList,jsList,diyworkList,frameworkList];  
 
 
 
