690a4712d614b1539a46cfe0fffd154111bf94c0
Leela Gutta <gutta.dhanya@gmail.com>
2016-08-17 07:32:44 -0700

    Adding links to all the resources

--- js/list.js
+++ js/list.js
@@ -1,95 +1,85 @@
 var introList=
-'<div class="intro"><p>These are few of the resources I always want to have in handy. This list will help anyone looking to kickstart their career in design in Tech.</p>';
+'<div class="intro"><h4>Kickstarter kit for beginners!! - This list will help anyone looking to kickstart their career in design in Tech.</h4>';
 
 var designList = 
 '<div class="heading"><h1>DESIGN</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Understanding what UX design is</li>'+
-'<li>Design and development job titles</li>'+
-'<li><a target="_blank" href="https://css-tricks.com/design-systems-building-future/">'+
-'Design and its role in Technology</a></li>'+
-'<li><a target="_blank" href="http://blog.crew.co/native-app-vs-web-app/">'+
-'what is design Thinking</a></li>'+
-'<li><a target="_blank" href="http://www.designyourway.net/blog/resources/understanding-design-and-development-jobs-titles/">'+
-'Design principals</a></li>'+
-'<li><a target="_blank" href="http://www.designyourway.net/blog/resources/understanding-design-and-development-jobs-titles/">'+
-'Best Design Books</a></li>'+
+'<li><a href="http://blog.careerfoundry.com/ui-design/the-difference-between-ux-and-ui-design-a-laymans-guide/">Difference between UX and UI Design</li>'+
+'<li><a target="_blank" href="http://uxmastery.com/7-signs-person-isnt-actually-ux-designer/">Whos and whos not an UX Designer?</a></li>'+
+'<li><a target="_blank" href="http://www.fastcompany.com/919258/design-thinking-what">Design Thinking</a></li>'+
+'<li><a target="_blank" href="http://www.digitalartsonline.co.uk/features/creative-business/6-best-ux-design-books/">Best Design Books</a></li>'+
 '</ul></div>'
 
 var responsiveList=
 '<div class="heading"><h1>Responsive DESIGN</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Responsive web design fundamentals - Udacity</li>'+
-'<li>Responsive images</li>'+
-'<li>What is viewport?</li>'+
-'<li><a href="http://learn.shayhowe.com/html-css/positioning-content/">'+
-'Positioning Content</a></li>'+
-'<li><a href="http://www.peterjthomson.com/2009/08/difference-between-marketing-and-design/">'+
-'Difference between marketing and Design</a></li></ul></div>';
+'<li><a target="_blank" href="https://www.udacity.com/course/responsive-web-design-fundamentals--ud893"</a>Responsive web design fundamentals - Udacity</li>'+
+'<li><a target="_black" href="https://www.udacity.com/course/responsive-images--ud882"</a>Responsive images</li>'+
+'<li><a target="_blank" href="http://blog.crew.co/native-app-vs-web-app/">Native app vs web app</a></li>'+
+'<li><a href="http://learn.shayhowe.com/html-css/positioning-content/">Positioning Content</a></li>';
 
 var researchTools=
 '<div class="heading"><h1>Research Tools</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Task Models</li>'+
-'<li>Content Audit</li>'+
-'<li>SiteMaps</li>'+
+'<li><a target="_blank" href="http://agile4ux.com/ux-design-techniques-task-model/">Task Models</a></li>'+
+'<li><a target="_blank" http://uxmastery.com/how-to-conduct-a-content-audit/">How to do a content Audit</a></li>'+
+'<li><a target="_blank"  href="https://blog.kissmetrics.com/build-a-sitemap/">Why you should build site maps before designing?</a></li>'+
 '<li><a target="_blank"  href="https://medium.com/@wnialloconnor/how-to-build-an-experience-map-5e55b7ee4f32#.h603ciorw">'+
 'How to build an experience map</a></li>'+
 '<li><a target="_blank"  href="https://www.smashingmagazine.com/2011/09/the-s-m-a-r-t-user-experience-strategy/">'+
 'SMART userExperience Strategy</a></li>'+
-'<li><a target="_blank"  href="https://www.smashingmagazine.com/2011/09/the-s-m-a-r-t-user-experience-strategy/">'+
+'<li><a target="_blank"  href="https://www.smashingmagazine.com/2014/08/a-closer-look-at-personas-part-1/">'+
 'User Personas</a></li></ul></div>';
 
 var colorList = 
 '<div class="heading"><h1>COLOR</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>What is color?</li>'+
-'<li>UI gradients</li>'+
-'<li>IOS colors</li>'+
-'<li>Color history</li>'+
-'<li>Color in web</li></ul></div>';
+'<li><a target="_blank" href="https://webflow.com/blog/web-design-101-color-theory">Color in web</a></li>'+
+'<li><a target="_blank" href="https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/">Color theory in IOS</a></li>'+
+'<li><a target="_blank" href="http://ios7colors.com/">IOS color swatches</a></li>'+
+'<li><a target="_blank" href="https://color.adobe.com/">Adobe Kuler</a></li></ul></div>';
+
 
 var typeList=
 '<div class="heading"><h1>TYPOGRAPHY</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Understanding Type</li>'+
-'<li>Best Type pairings</li>'+
-'<li>Typography in Web design</li>'+
-'<li>Best books in typography</li>'+
-'<li>Typography in mobile</li></ul></div>';
+'<li><a target="_blank" href="http://www.creativebloq.com/typography/20-perfect-type-pairings-3132120">Font Pairings</a></li>'+
+'<li><a target="_blank" href="http://www.webdesignerdepot.com/2009/02/10-web-typography-rules-every-designer-should-know-2/">Typography rules</a></li>'+
+'<li><a target="_blank" href="http://www.awwwards.com/websites/typography/"</a>Examples of typography</li>'+
+'<li><a target="_blank" href="https://www.smashingmagazine.com/2013/08/beautiful-typography-web-design/">A journey through beautiful typography</a></li>'+	
+'<li><a target="_blank" href="http://www.creativeworkline.com/2015/07/typography-in-mobile-apps/">Typography in mobile</a></li></ul></div>';
 
 var jsList=
 '<div class="heading"><h1>JavaScript</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Basics of JavaScript</li>'+
-'<li>Objects and Functions</li>'+
-'<li>How does memory work in JS</li>'+
-'<li>Scopes and clousers</li>'+
-'<li>Design Patterns</li>'+
-'<li>JQuery methods</li></ul></div>';
+'<li><a href="https://developer.mozilla.org/en-US/Learn/Getting_started_with_the_web/JavaScript_basics">Basics of JavaScript</a></li>'+
+'<li><a href="http://javascriptissexy.com/javascript-objects-in-detail/">Objects in Detail</a></li>'+
+'<li><a href="http://javascriptissexy.com/understand-javascript-closures-with-ease/">Scopes and clousers</a></li>'+
+'<li><a href="http://javascriptissexy.com/javascript-prototype-in-plain-detailed-language/">Prototypes</a></li>'+
+'<li><a href="http://shop.oreilly.com/product/0636920027065.do">Awesome book on JavaScript</a></li></ul></ul></div>';
 
 var htmlcssList=
 '<div class="heading"><h1>Html & CSS</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Css cascading rules</li>'+
-'<li>Box Model</li>'+
-'<li>Css flex-box</li>'+
-'<li>Css display</li>'+
-'<li>Animations</li>'+
+'<li><a href="http://monc.se/kitchen/38/cascading-order-and-inheritance-in-css">Css cascading rules</a></li>'+
+'<li><a href="http://learn.shayhowe.com/html-css/opening-the-box-model/"Box Model</a></li>'+
+'<li><a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/Css"flex-box</a></li>'+
+'<li><a href="https://css-tricks.com/almanac/properties/d/display/"Css display</a></li>'+
+'<li><a href="https://css-tricks.com/almanac/properties/a/animation/"Css Animations</a></li>'+
 '<li><a href="http://learnlayout.com/toc.html">CSS Layout</a></li></ul></div>';	
 
 var cssSelectors=
 '<div class="heading"><h1>Css Selectors</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Cheat sheet for css selectors</a></li>'+
+'<li><a href="http://www.w3schools.com/cssref/css_selectors.asp">Cheat sheet for css selectors</a></li>'+
 '<li><a href="http://www.webdesignerdepot.com/2013/08/10-css-selectors-you-shouldnt-code-without/">10 useful Selectors</a></li>'+
 '<li><a href="https://css-tricks.com/how-nth-child-works/">How n-th child works</a></li>'+
 '<li><a href="https://css-tricks.com/useful-nth-child-recipies/">n-th child receipes</a></li>'+
@@ -104,19 +94,16 @@ var diyworkList=
 '<li>Custom Grid</li>'+
 '<li>Custom Masonry Layout</li>'+
 '<li><a href="https://ole.michelsen.dk/blog/setup-local-web-server-apache-php-osx-yosemite.html">'+
-'Local WebServer Setup</a></li>'
-'<li>BootStrap</li></ul></div>';
+'Local WebServer Setup</a></li>';
 
 var frameworkList =
 '<div class="heading"><h1>Frameworks</h1></div>'+
 '<div class="line"></div>'+
 '<div class="list"><ul>'+
-'<li>Sass</li>'+
-'<li>Angular JS</li>'+
-'<li>Es Lint</li>'+
-'<li><a href="https://ole.michelsen.dk/blog/setup-local-web-server-apache-php-osx-yosemite.html">'+
-'Testing</a></li>'+
-'<li>BootStrap</li></ul></div>';
+'<li><a href="http://sass-lang.com/">Sass</a></li>'+
+'<li><a href="https://www.codeschool.com/courses/shaping-up-with-angular-js">Angular JS</a></li>'+
+'<li><a href="http://eslint.org/">Es Lint</a></li>'+
+'<li><a href="http://getbootstrap.com/css/">BootStrap</a></li>';
 
 
 var contactList='<ul>'+
@@ -126,7 +113,7 @@ var contactList='<ul>'+
 	'<li><a target="_blank" href="https://dhanyagutta.com"><img src="asserts/medium.png" height="30" width="30" alt="medium"></a></li>'+
 	'</ul>';
 
-var list =[introList,designList,responsiveList,researchTools,colorList,typeList,htmlcssList,cssSelectors,jsList,diyworkList,frameworkList];  
+var list =[introList,designList,responsiveList,researchTools,colorList,typeList,htmlcssList,cssSelectors,jsList,frameworkList];  
 
 
 
@@ -141,5 +128,5 @@ var list =[introList,designList,responsiveList,researchTools,colorList,typeList,
 	$(".box:nth-child(9)").append(list[8]); 
 	$(".box:nth-child(10)").append(list[9]); 
 	$(".box:nth-child(11)").append(list[10]);
-	$(".box:nth-child(12)").append(list[11]);   
+	// $(".box:nth-child(12)").append(list[11]);   
 	$(".contactList").append(contactList);
